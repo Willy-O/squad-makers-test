cbe5aa449dc892d0cdff209f27f6854971a78109
worozco <worozco@netnovation.com>
2022-09-21 12:03:18 -0400

    test: check it is working

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from routes import joke, math
 from fastapi import FastAPI
 
 app = FastAPI()
+
+app.include_router(joke.router)
+app.include_router(math.router)
+
+@app.get("/")
+async def root():
+  return {"message": "Hi from my api"}
 
