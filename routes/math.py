331d789ed638f273eb2c16b04870e8cc73760524
worozco <worozco@netnovation.com>
2022-09-21 12:26:27 -0400

    feat: add 'lcm' route

--- routes/math.py
+++ routes/math.py
@@ -1,3 +1,6 @@
+# from math import lcm
+from math import gcd
+
 from fastapi import APIRouter, Query
 
 router = APIRouter(
@@ -5,9 +8,21 @@ router = APIRouter(
 )
 
 @router.get("/plus-one")
-async def sum_one_to_current_number(number: int = Query(...)):
+async def plus_one_to_current_number(number: int = Query(...)):
   new_number = number + 1
   return {
     "detail": "success",
+    "message": "return the number sent whit plus '1'",
     "result": new_number
+  }
+
+@router.get("/lcm")
+async def least_commom_multiple(numbers: list[int] = Query(...)):
+  lcm = 1
+  for number in numbers:
+    lcm = lcm * number // gcd(lcm, number)
+  return {
+    "detail": "success",
+    "message": "return last common multiple the list sent",
+    "result": lcm
   }
