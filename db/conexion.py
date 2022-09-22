c7f771de0998c165c414852324efc7f9d37ed94c
worozco <worozco@netnovation.com>
2022-09-22 10:38:56 -0400

    feat: create db and conexion

---
+++ db/conexion.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+
+# Change the url_string if is neccesary with the follow format
+# mongodb://[username:password@]host1[:port1]
+DB_CONNECTION = 'mongodb://localhost:27017'
+
+class Conexion():
+  def __init__(self):
+    self.client = MongoClient(DB_CONNECTION)
+
+  def create_database(self):
+    try:
+      dblist = self.client.list_database_names()
+      if "squadMakers" in dblist:
+        return "The database exists."
+      else:
+        self.db = self.client["squadMakers"]
+        collection = self.db["joke"]
+        collection.insert_one({
+          "number": 1,
+          "text": "joke example",
+        })
+        # collist = self.db.list_collection_names()
+        return "The databases has been create"
+    except Exception as e:
+      print(type(e))
+      raise "error creating database"
