eec4bdea5ab7799222161b7001ae80640b0b72c2
worozco <worozco@netnovation.com>
2022-09-22 10:40:09 -0400

    feat: add function for create db on absolute route

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from routes import joke, math
 from fastapi import FastAPI
+from db.conexion import Conexion
 
 app = FastAPI()
 
@@ -8,5 +9,6 @@ app.include_router(math.router)
 
 @app.get("/")
 async def root():
+  obj_conexion = Conexion()
+  print(obj_conexion.create_database())
   return {"message": "Hi from my api"}
-
